0a9cb9d22c65740545d6889dc39754b22cacb176
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2022-06-04 17:09:17 -0500

    Initial Commit

---
+++ JP Apex Predators/ApexPredator.swift
@@ -0,0 +1,37 @@
+//
+//  ApexPredator.swift
+//  JP Apex Predators
+//
+//  Created by Jordan Nicodemus on 3/23/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ApexPredator: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let type: String
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    func typeOverlay() -> Color {
+        switch type {
+        case "land":
+            return .brown
+        case "air":
+            return .teal
+        case "sea":
+            return .blue
+        default:
+            return .brown
+        }
+    }
+}
+
+struct MovieScene: Codable, Identifiable {
+    let id: Int
+    let movie: String
+    let sceneDescription: String
+}
